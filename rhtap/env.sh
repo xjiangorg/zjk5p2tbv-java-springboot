871a461606b9b568ce34870ed409e816de4e24e2
Xin Jiang <53755997+xinredhat@users.noreply.github.com>
2025-03-03 15:43:36 +0800

    Update TUF mirror in environment file

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -47,7 +47,7 @@ export INFO=${INFO-true}
 export STRICT=${STRICT-true}
 export EFFECTIVE_TIME=${EFFECTIVE_TIME-now}
 export HOMEDIR=${HOMEDIR-$(pwd)}
-export TUF_MIRROR=${TUF_MIRROR-http://tuf.rhtap-tas.svc}
+export TUF_MIRROR=${TUF_MIRROR-https://tuf-rhtap-tas.apps.rosa.xjiang0303417.0lal.p3.openshiftapps.com}
 
 # Allow PR to succeed even if TAS vars not configured
 export FAIL_IF_TRUSTIFICATION_NOT_CONFIGURED=false
